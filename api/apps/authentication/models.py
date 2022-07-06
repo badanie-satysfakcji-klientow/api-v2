63075a37bedcbaf10fd8f6891968768cd7cb7e1f
Krystian Ogonowski <62177596+KrystianOg@users.noreply.github.com>
2022-07-06 12:09:58 +0200

    created user type

--- api/apps/authentication/models.py
+++ api/apps/authentication/models.py
@@ -5,12 +5,18 @@ from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+
+    class UserType(models.IntegerChoices):
+        INTERVIEWEE = (1, 'Interviewee')
+        CREATOR = (2, 'Creator')
+
     email = models.EmailField(max_length=320, unique=True)
     is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    type = models.IntegerField(choices=UserType.choices, default=UserType.INTERVIEWEE)
 
     USERNAME_FIELD = 'email'
 
@@ -22,5 +28,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     def tokens(self):
         # update that later
         pass
-
-
